83ba5e9b80091b3e753644a60e90d908effc4d3d
Radu <avadanii@tiscali.it>
2019-08-28 15:37:29 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <>
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <Logs />
+      </div>
+    </>
+  );
+};
+
+export default App;
